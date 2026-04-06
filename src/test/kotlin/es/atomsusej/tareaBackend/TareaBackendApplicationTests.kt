de2e45c8e12b5d81f1ccc5e1380111ac1ef311a1
motacj <motacj@madrid.es>
2026-04-06 11:54:23 +0200

    Pruebas terminadas y comprobadas. Cambios puntuales en el codigo

--- src/test/kotlin/es/atomsusej/tareaBackend/TareaBackendApplicationTests.kt
+++ src/test/kotlin/es/atomsusej/tareaBackend/TareaBackendApplicationTests.kt
@@ -2,12 +2,13 @@ package es.atomsusej.tareaBackend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TareaBackendApplicationTests {
 
 	@Test
 	fun contextLoads() {
 	}
-
 }
